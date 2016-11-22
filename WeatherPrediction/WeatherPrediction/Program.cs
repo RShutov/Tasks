401356e6ecc67ef8f13a063f1342df60e4e1110f
rshutov <rshutov@game-forest.com>
2016-11-22 13:08:13 +1000

    [WeatherPrediction]: Add model Initialization and ToString serialization

--- WeatherPrediction/WeatherPrediction/Program.cs
+++ WeatherPrediction/WeatherPrediction/Program.cs
@@ -12,6 +12,10 @@ namespace WeatherPrediction
 	{
 		static void Main(string[] args)
 		{
+			int count = 150;
+			int i = 0;
+			List<WeatherModel> trainingSet = new List<WeatherModel>();
+			List<WeatherModel> dataSet = new List<WeatherModel>();
 			using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\vlad_weather_data.csv"))
 			{
 				parser.TextFieldType = FieldType.Delimited;
@@ -19,10 +23,27 @@ namespace WeatherPrediction
 				string[] header = parser.ReadFields();
 				while (!parser.EndOfData)
 				{
+					if (i == count) break;
 					string[] fields = parser.ReadFields();
-					foreach (string field in fields)
-					{
+					string[] modelFields = {
+						fields[0],
+						fields[1],
+						fields[2],
+						fields[3],
+						fields[4],
+						fields[5],
+						fields[7],
+						fields[22],
+						fields[23],
+					};
+					var model = new WeatherModel(modelFields);
+					if (model.Date.Year == 2016) {
+						trainingSet.Add(model);
+						Console.WriteLine(model.ToString());
+					} else {
+						dataSet.Add(model);
 					}
+					i++;
 				}
 			}
 		}
--- WeatherPrediction/WeatherPrediction/WeatherModel.cs
+++ WeatherPrediction/WeatherPrediction/WeatherModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -62,5 +63,49 @@ namespace WeatherPrediction
 		/// <seealso cref="WeatherPrediction.WeatherModel"/>
 		/// </summary>
 		public double RRR { get; set; }
+
+		public static string DateFmt = "dd.MM.yyyy HH:mm";
+
+		public WeatherModel(string[] data)
+		{
+			try {
+				Date = DateTime.ParseExact(
+					data[0], DateFmt, 
+					System.Globalization.CultureInfo.InvariantCulture);
+				T = TryParse(data[1]);
+				Po  = TryParse(data[2]);
+				P   = TryParse(data[3]);
+				Pa  = TryParse(data[4]);
+				U   = TryParse(data[5]);
+				Ef  = TryParse(data[6]);
+				Td  = TryParse(data[7]);
+				RRR = TryParse(data[8]);
+			} catch(IndexOutOfRangeException) {
+				throw new Exception("wronng number params");
+			}
+		}
+
+		private double TryParse(string value)
+		{
+			try {
+				return Convert.ToDouble(value, CultureInfo.InvariantCulture);
+			} catch (FormatException) {
+				return 0;
+			}
+		}
+
+		public override string ToString()
+		{
+			return string.Join(" ", new string[]{
+				Date.ToString(DateFmt),
+				T.ToString(),
+				Po.ToString(),
+				Pa.ToString(),
+				U.ToString(),
+				Ef.ToString(),
+				Td.ToString(),
+				RRR.ToString(),
+			});
+		}
 	}
 }
